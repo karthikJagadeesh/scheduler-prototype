074960af3940afbbd56ee698c9c8a88f81882c7f
Karthik Jagadeesh <kartu8@gmail.com>
2016-12-12 14:19:02 +0530

    add context menu on daypilot

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
 const dayPilot = new DayPilot.Scheduler('dp')
 dayPilot.cssClassPrefix = "dp"
+dayPilot.clipBoard = null
 
 // resources related  -------------------------------------------------------------
 dayPilot.treeEnabled = true
-dayPilot.resources = [
+const resources = dayPilot.resources = [
     {
         name: "Task 1",
         id: "task1",
@@ -64,6 +65,7 @@ dayPilot.separators = [
 
 //-------------------------------------------------------------
 
+//predefined events --------------------------------
 dayPilot.events.list = [
     {
         start: "2016-01-04",
@@ -71,29 +73,150 @@ dayPilot.events.list = [
         id: "1",
         resource: "maxj",
         text: "1's,A Stitch, 1041, 40 ",
-        total: 13,
+        //total: 1, //no of work hours
         barColor: "#9a0"
     }
 ];
 
+//-------------------------------------------------
+
+
+
+//on click and drag to create a new event------------------------
 dayPilot.onTimeRangeSelected = args => {
-    const name = prompt("Enter new event name", "Event")
-    dayPilot.clearSelection()
+  console.log(args)
+    let modal = new DayPilot.Modal()
+    modal.top = 160
+    modal.width = 300
+    modal.opacity = 70
+    modal.border = "2px solid rgba(0,0,0,0.2)"
+    modal.zIndex = 100
+    modal.showHtml()
 
-    if (!name)
-        return
 
-    const e = new DayPilot.Event({start: args.start, end: args.end, id: DayPilot.guid(), resource: args.resource, text: name})
+    const name = document.querySelector("#event-name").value
+
+    const e = new DayPilot.Event({
+      start: args.start,
+      end: args.end,
+      id: DayPilot.guid(),
+      resource: args.resource,
+      text: name
+    })
 
     dayPilot.events.add(e)
     dayPilot.message("Created")
 
 }
+//------------------------
+
+
+//right click on an event-------------------------------------------
+dayPilot.contextMenu = new DayPilot.Menu({
+  items: [
+    {
+      text: "Edit",
+      onclick: function() {
+
+      }
+    },
+    {
+      text: "Delete",
+      onclick: function() {
+        const response = confirm("Are you sure you want to delete?")
+        if (response)
+          dayPilot.events.remove(this.source)
+      }
+    },
+    {
+      text: "Copy",
+      onclick: function() {
+        dayPilot.clipBoard = this.source
+      }
+    },
+    {
+      text: "Select",
+      onclick: function() {
+        dayPilot.multiselect.add(this.source)
+      }
+    },
+    {
+      text: "Re-Assign To",
+      items: resources[0].children
+    }
+  ]
+})
+//------------------------------------------------------
+
+// right click on empty block---------------------------
+dayPilot.contextMenuSelection = new DayPilot.Menu({
+  items: [
+    {
+      text: "paste",
+      onclick: function() {
+        if (!dayPilot.clipBoard)
+          alert("Nothing copied to paste")
+
+        const selection = this.source
+        const { clipBoard } = dayPilot
+        const duration = clipBoard.end().getTime() - clipBoard.start().getTime()
+
+        const newEvent = new DayPilot.Event({
+          start: selection.start,
+          end: selection.start.addMilliseconds(duration),
+          text: clipBoard.text(),
+          resource: selection.resource,
+          id: DayPilot.guid()
+        })
+
+        dayPilot.events.add(newEvent)
+        dayPilot.clipBoard = null
+      }
+    }
+  ]
+})
+//--------------------------------------------------------
+
+// let modal = new DayPilot.Modal()
+// modal.top = 160
+// modal.width = 300
+// modal.opacity = 70
+// modal.border = "2px solid rgba(0,0,0,0.2)"
+// modal.zIndex = 100
+// modal.showHtml(`
+//   <div>Hello</div>
+//   <div>there</div>
+//   `)
+
+// dayPilot.bubble = new DayPilot.Bubble({
+//
+//   }
+// })
 
 dayPilot.cellWidth = 50
 dayPilot.headerHeight = 40
 dayPilot.rowHeight = 40
 dayPilot.eventHeight = 40
-//dayPilot.rowMarginTop = 10
+
+
+// function gridCalendarHeight() {
+//   var calendarContainer = document.querySelector('#calendar-container');
+//   var footer = document.querySelector('.footer')
+//   var windowHeight = window.innerHeight;
+//
+//   var getOffsetTop = function(el) {
+//     return el.getBoundingClientRect().top + window.scrollY;
+//   };
+//
+//   var totalHeight = calendarContainer.style.height +
+//                     getOffsetTop(calendarContainer) +
+//                     footer.style.height;
+//
+//   if (totalHeight >= windowHeight) {
+//     dp.heightSpec = 'Fixed';
+//     dp.height = windowHeight - getOffsetTop(calendarContainer) - footer.style.height - 80;
+//     dp.update();
+//   }
+// }
 
 dayPilot.init()
